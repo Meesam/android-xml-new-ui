4166b66dbe8034fb229bc7be3e7a93f5951d8fe3
Meesam Zaidi <meesam.engineer@gmail.com>
2024-10-08 09:47:08 +0530

    Working on Login Screen

--- app/src/main/java/com/xml_new_ui/myapplication/MainActivity.kt
+++ app/src/main/java/com/xml_new_ui/myapplication/MainActivity.kt
@@ -17,8 +17,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        setSupportActionBar(findViewById(R.id.app_toolbar))
-
-
     }
 }
